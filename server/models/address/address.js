95454af797e877a6b1e59954b8269ddd6d405806
OfcaPL <lukaszkups@gmail.com>
2015-01-08 21:27:38 +0100

    initial commit for github

--- server/models/address/address.js
+++
@@ -1,6 +0,0 @@
-Address = new Meteor.Collection('address');
-
-Meteor.publish('address', function(){
-	return Meteor.address.find({}, {fields:{street:1, house_no:1, postal:1, city:1, country:1}});
-});
-
--- server/models/kolory/kolory.js
+++
@@ -1,6 +0,0 @@
-Kolory = new Meteor.Collection('kolory');
-
-Meteor.publish('kolory', function(){
-	return Meteor.kolory.find({}, {fields:{kolor:1, czerwony,:1, czarny]:1}});
-});
-
